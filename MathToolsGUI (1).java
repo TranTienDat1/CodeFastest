42498ecc537c01c8afeb4fdbfbad19c085a1d7e1
AlanTruong43 <125585910+AlanTruong43@users.noreply.github.com>
2025-05-21 08:51:03 +0700

    Update MathToolsGUI (1).java
    add giải pt bậc 1 + số đối xứng

--- MathToolsGUI (1).java
+++ MathToolsGUI (1).java
@@ -184,7 +184,15 @@ public class MathToolsGUI extends JFrame {
      * @return ƯCLN của a và b
      */
     private int findGCD(int a, int b) {
-//--
+                a = Math.abs(a);
+        b = Math.abs(b);
+        
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 
     /**
